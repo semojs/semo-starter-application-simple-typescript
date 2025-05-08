f7c65fa68113bde7ca98bd43c34ba902f1212561
Richard Yu <vipzhicheng@gmail.com>
2025-05-08 15:13:26 +0800

    change from cjs to esm, and update deps and fix a lot issues

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import http from 'http'
+import http from "node:http";
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
 });
 
 server.listen(port, hostname, () => {
